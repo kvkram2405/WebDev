25d51eb73b9da0525d9bc0ece90e3463d93aefc2
kvkram2405 <kvkram2405gmail.com>
2020-01-22 14:21:13 +0530

    Password Hash added !

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
-       "fmt"
-"github.com/gorilla/mux"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func dbConn() (db *sql.DB) {
@@ -21,30 +23,50 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 }
 
-func web (w http.ResponseWriter, r *http.Request) {
-      fmt.Fprintf(w, "Welcome to my website!")
- }
+func web(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my website!")
+}
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 15)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func password(w http.ResponseWriter, r *http.Request) {
+
+	password := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+
+}
 
 func main() {
-r := mux.NewRouter()
+	r := mux.NewRouter()
 	log.Println("Server started on: http://localhost:3000")
 	r.HandleFunc("/", Index)
-r.HandleFunc("/web", web)
+	r.HandleFunc("/web", web)
+	r.HandleFunc("/auth", password)
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
 
- r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["title"]
-        page := vars["page"]
-
-        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-    })
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
 
 	http.ListenAndServe(":3000", r)
 }
-
